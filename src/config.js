f1f5c4c87d08129d0ea293c5ea3d382519633d57
erikcorea <erikcorrea444@gmail.com>
2020-12-18 08:18:58 -0600

    Config file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const apiUrl =
 	window.location.hostname === 'localhost'
 		? 'http://localhost:8000'
-		: 'https://code-connections-be.herokuapp.com/';
+		: 'https://code-connections-be.herokuapp.com';
